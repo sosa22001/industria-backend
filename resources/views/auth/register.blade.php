6994d8b1ac8a06ffa06f6eb91cf7fe3055cd87f8
Esdras <esdras.castillo@unah.hn>
2024-10-24 13:32:03 -0600

    actualizacion de composer

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -49,3 +49,13 @@
     </div>
 </div>
 @endsection
+
+@section('head')
+    <!-- Flowbite CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+@endsection
+
+@section('scripts')
+    <!-- Flowbite JS -->
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+@endsection
